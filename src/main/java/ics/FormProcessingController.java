604cd84de56c3d442f0185246a7170b0797a2a29
KateBashkirova <3.14e@mail.ru>
2020-08-26 03:15:39 +0600

    Fixed file structure.

--- src/main/java/ics/FormProcessingController.java
+++ src/main/java/ics/FormProcessingController.java
@@ -1,28 +1,22 @@
 package ics;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import ics.icsClasses.DateAndTime;
-import ics.icsClasses.MeetingProcessor;
 import ics.icsClasses.Reminder;
 import ics.icsClasses.Timezone;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.w3c.dom.ls.LSOutput;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
 import java.util.ArrayList;
 
 @Controller
 public class FormProcessingController {
+    //отображение страницы с формой
     @RequestMapping(value = "/createMeeting", method = RequestMethod.GET)
     public ModelAndView showMeetingForm()
     {
@@ -31,232 +25,70 @@ public class FormProcessingController {
         return modelAndView;
     }
 
-  /*  //формирование файла
-    @RequestMapping(value = "/readFile")
-    @ResponseBody
-    public String readFile() throws IOException {
-        String response;
-
-        //подключаем классы для работы
-        MeetingProcessor mt = new MeetingProcessor();
-        Meeting meeting = mt.processJson();
-        Timezone tz = new Timezone();
-        DateAndTime dateAndTime = new DateAndTime();
-        ArrayList <String> currDateAndTime = dateAndTime.currentDateAndTime();
-        Reminder reminder = new Reminder();
-
-        //пробуем создать файл
-        File dir = new File("D://Универчик//Практика//newVersion//file");
-        File meetingFile = new File(dir, meeting.getSummary() + ".ics");
-        if (!meetingFile.createNewFile())
-        {
-            response = "Cannot create file";
-        }
-        else response = "File with your meeting has been created";
-
-        //заполняем файл
-        FileWriter writer = new FileWriter(meetingFile);
-        writer.write("BEGIN:VCALENDAR\n" +
-                "VERSION:2.0\n" +
-                "PRODID:somerandomgirl\n" +
-                "CALSCALE:GREGORIAN\n" +
-                "BEGIN:VTIMEZONE\n" +
-                "TZID=" + tz.tzid() + "\n" +
-                "TZURL:http://tzurl.org/zoneinfo-outlook/" + tz.tzid() + "\n" +
-                "X-LIC-LOCATION:" + tz.tzid() + "\n" +
-                "BEGIN:STANDARD\n" +
-                "TZOFFSETFROM:" + tz.tzOffSet() + "\n" +
-                "TZOFFSETTO:" + tz.tzOffSet() + "\n" +
-                "TZNAME:" + tz.tzName() + "\n" +
-                "DTSTART:19700101T000000\n" +
-                "END:STANDARD\n" +
-                "END:VTIMEZONE\n" +
-                "BEGIN:VEVENT\n" +
-                "DTSTAMP:" + currDateAndTime.get(0) + "T" + currDateAndTime.get(1) + "Z" + "\n" +
-                "DTSTART;TZID=" + tz.tzid() + ":" + dateAndTime.Date("start") + "T" + dateAndTime.Time("start") + "\n" +
-                "DTEND;TZID=" + tz.tzid() + ":" + dateAndTime.Date("end") + "T" + dateAndTime.Time("end") + "\n" +
-                "SUMMARY:" + meeting.getSummary() + "\n" +
-                "DESCRIPTION:" + meeting.getDescription() + "\n" +
-                "LOCATION:" + meeting.getLocation() + "\n");
-        if(meeting.getEventStatus().equalsIgnoreCase("Out Of Office")){
-            writer.write("X-MICROSOFT-CDO-BUSYSTATUS:" + "OOF" + "\n");
-        }
-        else writer.write("X-MICROSOFT-CDO-BUSYSTATUS:" + meeting.getEventStatus().toUpperCase() + "\n");
-        //если нужно напоминание о встрече
-        if (!reminder.ReminderTime().equalsIgnoreCase("null"))
-        {
-            writer.write("BEGIN:VALARM\n" +
-                            "ACTION:DISPLAY\n" +
-                            "DESCRIPTION:" + meeting.getDescription() +"\n" +
-                            "TRIGGER:" + reminder.ReminderTime() + "\n" +
-                            "END:VALARM\n" + "END:VEVENT\n" + "END:VCALENDAR\n");
-        }
-        else {
-            writer.write("END:VEVENT\n" + "END:VCALENDAR\n");
-        }
-        writer.close();
-        return response;
-    } */
-
-
-    //FIXME: error 415 при том, что meeting благополучно парсит. Content-Type 100%
     @RequestMapping(value="/createMeeting", method = RequestMethod.POST, consumes = "application/json")
     //тут как бы meeting, но на деле meeting = пришедшей jsonStr, просто Meeting показывает, что jsonStr распарсить по Meeting.class
-    public ResponseEntity createMeeting(@RequestBody Meeting meeting) throws IOException {
-        System.out.println(meeting.getSummary());
-        System.out.println(meeting.getLocation());
-        System.out.println(meeting.getStartDate());
-        System.out.println(meeting.getStartTime());
-        System.out.println(meeting.getEventStatus());
-
+    public ResponseEntity createMeeting(@RequestBody Meeting meeting) {
         Timezone tz = new Timezone();
         DateAndTime dateAndTime = new DateAndTime();
         ArrayList <String> currDateAndTime = dateAndTime.currentDateAndTime();
         Reminder reminder = new Reminder();
 
-        String meetingInfo = "BEGIN:VCALENDAR\n" +
-                "VERSION:2.0\n" +
-                "PRODID:ktbrv\n" +
-                "CALSCALE:GREGORIAN\n" +
-                "BEGIN:VTIMEZONE\n" +
-                "TZID=" + tz.tzid(meeting.getTimezone()) + "\n" +
-                "TZURL:http://tzurl.org/zoneinfo-outlook/" + tz.tzid(meeting.getTimezone()) + "\n" +
-                "X-LIC-LOCATION:" + tz.tzid(meeting.getTimezone()) + "\n" +
-                "BEGIN:STANDARD\n" +
-                "TZOFFSETFROM:" + tz.tzOffSet(meeting.getTimezone()) +
-                "TZOFFSETTO:" + tz.tzOffSet(meeting.getTimezone()) +
-                "TZNAME:" + tz.tzName(meeting.getTimezone()) +
-                "DTSTART:19700101T000000" +
-                "END:STANDARD" +
-                "END:VTIMEZONE" +
-                "BEGIN:VEVENT" +
-                "DTSTAMP:" + currDateAndTime.get(0) + "T" + currDateAndTime.get(1) + "Z" + "\n" +
-                "DTSTART;TZID=" + tz.tzid(meeting.getTimezone()) + ":" + dateAndTime.Date(meeting.getStartDate()) + "T" + dateAndTime.Time(meeting.getStartTime()) + "\n" +
-                "DTEND;TZID=" + tz.tzid(meeting.getTimezone()) + ":" + dateAndTime.Date(meeting.getEndDate()) + "T" + dateAndTime.Time(meeting.getEndTime()) + "\n" +
-                "SUMMARY:" + meeting.getSummary() + "\n" +
-                "DESCRIPTION:" + meeting.getDescription() + "\n" +
-                "LOCATION:" + meeting.getLocation() + "\n";
-
-        if(meeting.getEventStatus().equalsIgnoreCase("Out Of Office")){
-             meetingInfo.concat("X-MICROSOFT-CDO-BUSYSTATUS:OOF");
+        ArrayList<String> meetingInfo = new ArrayList<>();
+        meetingInfo.add("BEGIN:VCALENDAR\n" +
+                        "VERSION:2.0\n" +
+                        "PRODID:ktbrv\n" +
+                        "CALSCALE:GREGORIAN\n" +
+                        "BEGIN:VTIMEZONE\n" +
+                        "TZID=" + tz.tzid(meeting.getTimezone()) + "\n" +
+                        "TZURL:http://tzurl.org/zoneinfo-outlook/" + tz.tzid(meeting.getTimezone()) + "\n" +
+                        "X-LIC-LOCATION:" + tz.tzid(meeting.getTimezone()) + "\n" +
+                        "BEGIN:STANDARD\n" +
+                        "TZOFFSETFROM:" + tz.tzOffSet(meeting.getTimezone()) + "\n" +
+                        "TZOFFSETTO:" + tz.tzOffSet(meeting.getTimezone()) + "\n" +
+                        "TZNAME:" + tz.tzName(meeting.getTimezone()) + "\n" +
+                        "DTSTART:19700101T000000\n" +
+                        "END:STANDARD\n" +
+                        "END:VTIMEZONE\n" +
+                        "BEGIN:VEVENT\n" +
+                        "DTSTAMP:" + currDateAndTime.get(0) + "T" + currDateAndTime.get(1) + "Z" + "\n" +
+                        "DTSTART;TZID=" + tz.tzid(meeting.getTimezone()) + ":" + dateAndTime.Date(meeting.getStartDate())
+                                        + "T" + dateAndTime.Time(meeting.getStartTime()) + "\n" +
+                        "DTEND;TZID=" + tz.tzid(meeting.getTimezone()) + ":" + dateAndTime.Date(meeting.getEndDate())
+                                        + "T" + dateAndTime.Time(meeting.getEndTime()) + "\n" +
+                        "SUMMARY:" + meeting.getSummary() + "\n");
+        if (!meeting.getDescription().trim().isEmpty()){
+            meetingInfo.add("DESCRIPTION:" + meeting.getDescription() + "\n");
         }
-        else meetingInfo.concat("X-MICROSOFT-CDO-BUSYSTATUS:" + meeting.getEventStatus().toUpperCase());
-
+                        //"DESCRIPTION:" + meeting.getDescription() + "\n" +
+        meetingInfo.add("LOCATION:" + meeting.getLocation() + "\n");
+        //проверка статуса мероприятия
+        if(meeting.getEventStatus().equalsIgnoreCase("Free")){
+            meetingInfo.add("TRANSP:TRANSPARENT\n");
+        }
+        else meetingInfo.add("TRANSP:OPAQUE\n");
         //если нужно напоминание о встрече
         if (!reminder.ReminderTime(meeting.getReminder()).equalsIgnoreCase("null"))
         {
-            meetingInfo.concat("BEGIN:VALARM" +
-                    "ACTION:DISPLAY" +
-                    "DESCRIPTION:" + meeting.getDescription() +
-                    "TRIGGER:" + reminder.ReminderTime(meeting.getReminder()) +
-                    "END:VALARM" + "END:VEVENT" + "END:VCALENDAR");
+            meetingInfo.add("BEGIN:VALARM\n" + "ACTION:DISPLAY\n" + "DESCRIPTION:reminder\n");
+            /*if (!meeting.getDescription().trim().isEmpty()){
+                meetingInfo.add("DESCRIPTION:" + meeting.getDescription());
+            }*/
+            meetingInfo.add("TRIGGER:" + reminder.ReminderTime(meeting.getReminder()) + "\n" +
+                            "END:VALARM\n" + "END:VEVENT\n" + "END:VCALENDAR\n");
         }
         else {
-            meetingInfo.concat("END:VEVENT" + "END:VCALENDAR");
+            meetingInfo.add("END:VEVENT\n" + "END:VCALENDAR");
         }
 
+        //ArrayList -> String
+        String joined = String.join("", meetingInfo);
+
         //формирование файла и отправка на скачивание
-        ByteArrayResource bt = new ByteArrayResource(meetingInfo.getBytes()); //предпочтительней, судя по spring.io
+        ByteArrayResource bt = new ByteArrayResource(joined.getBytes());
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.valueOf("application/ics"));
         headers.add("Content-Disposition", "attachment;filename=" + meeting.getSummary() + ".ics");
         return new ResponseEntity(bt, headers, HttpStatus.OK);
     }
-
-    //формируем и отдаём файл "на ходу"
-    @RequestMapping(value = "/download", method = RequestMethod.GET)
-    //ResponseEntity aka полный HTTP ответ
-    public ResponseEntity<String> download() throws IOException {
-        String meetingInfo = "BEGIN:VCALENDAR\n" +
-                "VERSION:2.0\n" +
-                "PRODID:betterNotToKnow\n" +
-                "CALSCALE:GREGORIAN\n" +
-                "BEGIN:VTIMEZONE\n" +
-                "TZID:Asia/Singapore\n" +
-                "TZURL:http://tzurl.org/zoneinfo-outlook/Asia/Singapore\n" +
-                "X-LIC-LOCATION:Asia/Singapore\n" +
-                "BEGIN:STANDARD\n" +
-                "TZOFFSETFROM:+0800\n" +
-                "TZOFFSETTO:+0800\n" +
-                "TZNAME:+08\n" +
-                "DTSTART:19700101T000000\n" +
-                "END:STANDARD\n" +
-                "END:VTIMEZONE\n" +
-                "BEGIN:VEVENT\n" +
-                "DTSTAMP:20200726T192050Z\n" +
-                "DTSTART;TZID=Asia/Singapore:20200713T120000\n" +
-                "DTEND;TZID=Asia/Singapore:20200713T120000\n" +
-                "SUMMARY:Some meeting\n" +
-                "LOCATION:Omsk\n" +
-                "X-MICROSOFT-CDO-BUSYSTATUS:OOF\n" +
-                "END:VEVENT\n" +
-                "END:VCALENDAR";
-       // InputStream is = new ByteArrayInputStream(meetingInfo.getBytes()); //преобразуем строку в поток ввода
-        //InputStreamResource isResource = new InputStreamResource(is);
-        ByteArrayResource bt = new ByteArrayResource(meetingInfo.getBytes()); //предпочтительней, судя по spring.io
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.valueOf("application/ics"));
-        headers.add("Content-Disposition", "attachment;filename=meeting.ics");
-        return new ResponseEntity(bt, headers, HttpStatus.OK);
-    }
-
-       /* Timezone tz = new Timezone();
-        DateAndTime dateAndTime = new DateAndTime();
-        ArrayList <String> currDateAndTime = dateAndTime.currentDateAndTime();
-        Reminder reminder = new Reminder();
-
-        //пробуем создать файл
-        File dir = new File("D://Универчик//Практика//newVersion//file");
-        File meetingFile = new File(dir, meeting.getSummary() + ".ics");
-        if (!meetingFile.createNewFile())
-        {
-            response = "Cannot create file";
-        }
-        else response = "File with your meeting has been created";
-
-        //заполняем файл
-        FileWriter writer = new FileWriter(meetingFile);
-        writer.write("BEGIN:VCALENDAR\n" +
-                "VERSION:2.0\n" +
-                "PRODID:somerandomgirl\n" +
-                "CALSCALE:GREGORIAN\n" +
-                "BEGIN:VTIMEZONE\n" +
-                "TZID=" + tz.tzid(meeting.getTimezone()) + "\n" +
-                "TZURL:http://tzurl.org/zoneinfo-outlook/" + tz.tzid(meeting.getTimezone()) + "\n" +
-                "X-LIC-LOCATION:" + tz.tzid(meeting.getTimezone()) + "\n" +
-                "BEGIN:STANDARD\n" +
-                "TZOFFSETFROM:" + tz.tzOffSet(meeting.getTimezone()) + "\n" +
-                "TZOFFSETTO:" + tz.tzOffSet(meeting.getTimezone()) + "\n" +
-                "TZNAME:" + tz.tzName(meeting.getTimezone()) + "\n" +
-                "DTSTART:19700101T000000\n" +
-                "END:STANDARD\n" +
-                "END:VTIMEZONE\n" +
-                "BEGIN:VEVENT\n" +
-                "DTSTAMP:" + currDateAndTime.get(0) + "T" + currDateAndTime.get(1) + "Z" + "\n" +
-                "DTSTART;TZID=" + tz.tzid(meeting.getTimezone()) + ":" + dateAndTime.Date(meeting.getStartDate()) + "T" + dateAndTime.Time(meeting.getStartTime()) + "\n" +
-                "DTEND;TZID=" + tz.tzid(meeting.getTimezone()) + ":" + dateAndTime.Date(meeting.getEndDate()) + "T" + dateAndTime.Time(meeting.getEndTime()) + "\n" +
-                "SUMMARY:" + meeting.getSummary() + "\n" +
-                "DESCRIPTION:" + meeting.getDescription() + "\n" +
-                "LOCATION:" + meeting.getLocation() + "\n");
-        if(meeting.getEventStatus().equalsIgnoreCase("Out Of Office")){
-            writer.write("X-MICROSOFT-CDO-BUSYSTATUS:" + "OOF" + "\n");
-        }
-        else writer.write("X-MICROSOFT-CDO-BUSYSTATUS:" + meeting.getEventStatus().toUpperCase() + "\n");
-        //если нужно напоминание о встрече
-        if (!reminder.ReminderTime(meeting.getReminder()).equalsIgnoreCase("null"))
-        {
-            writer.write("BEGIN:VALARM\n" +
-                    "ACTION:DISPLAY\n" +
-                    "DESCRIPTION:" + meeting.getDescription() +"\n" +
-                    "TRIGGER:" + reminder.ReminderTime(meeting.getReminder()) + "\n" +
-                    "END:VALARM\n" + "END:VEVENT\n" + "END:VCALENDAR\n");
-        }
-        else {
-            writer.write("END:VEVENT\n" + "END:VCALENDAR\n");
-        }
-        writer.close();
-        System.out.println(tz.tzid(meeting.getTimezone()));
-        System.out.println(meeting.getSummary());
-    } */
 }
 
--- src/main/webapp/WEB-INF/views/meeting.jsp
+++ src/main/webapp/WEB-INF/views/meeting.jsp
@@ -20,31 +20,22 @@
         var json = JSON.stringify(object); //преобразуем в JSON-строку
         var request = new XMLHttpRequest();
         request.open("POST","./createMeeting",true);
-        request.responseType = "blob";
+        request.responseType = "blob"; //blob - незименяемые, необработанные данные
         request.setRequestHeader("Content-Type", "application/json");
+        //определение функции-обработчика ответа (не вызов!). Своеобразный handle, оболочка для помещения ответа.
+        //эта функция будет вызвана после send() (или во время), но определена должна быть ДО, чтобы быть тут, когда
+        //она понадобится xhr
         request.onload = function() {
-            var blob = request.response;
-            var link = document.createElement('a');
-            link.href = window.URL.createObjectURL(blob);
-            link.download = 'meeting.ics';
-            link.click();
+            var blob = request.response; //получаем ответ сервера
+            var link = document.createElement('a'); //создаём элемент
+            link.href = window.URL.createObjectURL(blob); //привязываем на него URL ответа - сгенерированного сервером файла
+            let fileName = document.forms.newMeetingForm.elements.summary.value + ".ics"; //назвать файл по summary из формы
+            link.download = fileName;
+            link.click(); //триггернуть нажатие
         }
         request.send(json);
         alert("JSON created / createJSON works");
         return false;
-    //    $.ajax({
-    //         type: 'POST',
-    //         url: './createMeeting',
-    //         data: jsonStr,
-    //         contentType: "application/json; charset=utf-8",
-    //         dataType: "json",
-    //         async: false,
-    //         cache: false,
-    //         onData: function(data) {
-    //             alert("ajax works");
-    //             location = data;
-    //         }
-    // });
     }
 </script>
 <div class="container-xl">
@@ -52,10 +43,10 @@
         <label style="font-size: 20px;">New Meeting</label>
         <div class="form-group">
             <label for="summary" style="color: aqua;">Event title</label>
-            <input name="summary" type="text" class="form-control" id="summary" required>
+            <input name="summary" type="text" class="form-control" id="summary" value="Meeting with client" required>
             <br>
             <label for="location" id="locationLb">Location</label>
-            <input name="location" type="text" class="form-control" id="location">
+            <input name="location" type="text" class="form-control" value="Omsk" id="location">
             <br>
             <label for="description">Description</label>
             <textarea name="description" class="form-control" id="description" rows="5"></textarea>
@@ -70,12 +61,12 @@
                 <div class="form-group col-md-6">
                     <label for="startDate">Start date</label>
                     <small id="startDateHelp" class="form-text text-muted">Enter data as DD.MM.YYYY</small>
-                    <input name="startDate" type="date" class="form-control" id="startDate" pattern="(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}" required>
+                    <input name="startDate" type="date" class="form-control" id="startDate" value="2020-08-27" pattern="(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}" required>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="startTime">Start time</label>
                     <small id="startTimeHelp" class="form-text text-muted">Enter data as HH:MM</small>
-                    <input name="startTime" type="time" class="form-control" id="startTime" pattern="(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}" required>
+                    <input name="startTime" type="time" class="form-control" id="startTime" value="22:00" pattern="(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}" required>
                 </div>
             </div>
             <br>
@@ -83,12 +74,12 @@
                 <div class="form-group col-md-6">
                     <label for="endDate">End date</label>
                     <small id="endDateHelp" class="form-text text-muted">Enter data as DD.MM.YYYY</small>
-                    <input name="endDate" type="date" class="form-control" id="endDate" pattern="(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}" required>
+                    <input name="endDate" type="date" class="form-control" id="endDate" value="2020-08-28" pattern="(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}" required>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="endTime">End time</label>
                     <small id="endTimeHelp" class="form-text text-muted">Enter data as HH:MM</small>
-                    <input name="endTime" type="time" class="form-control" id="endTime" pattern="(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}" required>
+                    <input name="endTime" type="time" class="form-control" id="endTime" value="23:00" pattern="(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}" required>
                 </div>
             </div>
             <br>
@@ -136,8 +127,6 @@
             <select name="eventStatus" id="selectEventStatus" class="form-control">
                 <option selected>Free</option>
                 <option>Busy</option>
-                <option>Tentative</option>
-                <option>Out Of Office</option>
             </select>
         </div>
         <button id="submit" type="submit" class="btn btn-primary">Create ics file</button>
