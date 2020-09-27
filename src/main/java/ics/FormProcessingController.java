1dcbe282d8cd7bbf9c2f64465b85c6ea947fd8e8
KateBashkirova <kath.bashkirova@gmail.com>
2020-09-27 22:32:20 +0600

    Added class Builder. The lines for the file configuration have been moved from the controller in there.

--- src/main/java/ics/FormProcessingController.java
+++ src/main/java/ics/FormProcessingController.java
@@ -1,6 +1,5 @@
 package ics;
 
-import ics.icsClasses.EventStatus;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,11 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.Date;
-
 import static ics.icsClasses.FormatHelper.*;
-import static ics.icsClasses.Reminder.*;
 
 /**
  * The main controller
@@ -40,51 +35,14 @@ public class FormProcessingController {
      */
     @RequestMapping(value = "/createMeeting", method = RequestMethod.POST, consumes = "application/json")
     public ResponseEntity createMeeting(@RequestBody Meeting meeting) {
-        ArrayList<String> meetingInfo = new ArrayList<>();
         //todo extract to Builder
-        meetingInfo.add("BEGIN:VCALENDAR\r\n" +
-                "VERSION:2.0\r\n" +
-                "PRODID:ktbrv\r\n" +
-                "CALSCALE:GREGORIAN\r\n" +
-                "BEGIN:VTIMEZONE\r\n" +
-                "TZID=" + getTimezoneId(meeting) + "\r\n" +
-                "X-LIC-LOCATION:" + getTimezoneId(meeting) + "\r\n" +
-                "BEGIN:STANDARD\r\n" +
-                "TZOFFSETFROM:" + getTimezoneOffSet(meeting) + "\r\n" +
-                "TZOFFSETTO:" + getTimezoneOffSet(meeting) + "\r\n" +
-                "DTSTART:19700101T000000\r\n" +
-                "END:STANDARD\r\n" +
-                "END:VTIMEZONE\r\n" +
-                "BEGIN:VEVENT\r\n" +
-                "DTSTAMP:" + formatDate(DATE_T_TIME_Z, new Date()) + "\r\n" +
-                "DTSTART;TZID=" + getStartDateString(meeting) + "\r\n" +
-                "DTEND;TZID=" + getEndDateString(meeting) + "\r\n" +
-                "SUMMARY:" + meeting.getSummary() + "\r\n");
-        //если у мероприятия есть описание
-        if (!meeting.getDescription().trim().isEmpty()) {
-            meetingInfo.add("DESCRIPTION:" + meeting.getDescription() + "\r\n");
-        }
-        meetingInfo.add("LOCATION:" + meeting.getLocation() + "\r\n");
-
-        //добавить статус мероприятия
-        EventStatus eventStatus = EventStatus.valueOf(
-                meeting.getEventStatus().replace(" ", "_").toUpperCase()
-        );
-        meetingInfo.add(eventStatus.getConfig());
-
-        //добавить напоминание о встрече (если нужно)
-        if (meeting.getReminder().isOn()) {
-            meetingInfo.add("BEGIN:VALARM\r\n" + "ACTION:DISPLAY\r\n");
-            meetingInfo.add("TRIGGER:" + meeting.getReminder().getReminderText());
-            meetingInfo.add("END:VALARM\r\n");
-        }
-        meetingInfo.add("END:VEVENT\r\n" + "END:VCALENDAR");
-
-        //преобразовать ArrayList в String
-        String completeMeetingInfo = String.join("", meetingInfo);
-
         //сформировать и вернуть файл
-        ByteArrayResource bt = new ByteArrayResource(completeMeetingInfo.getBytes());
+        Builder builder = new Builder();
+        String fileConfig = builder.buildFileConfig(getTimezoneId(meeting), getTimezoneOffSet(meeting),
+                getStartDateString(meeting), getEndDateString(meeting), meeting.getSummary(), meeting.getDescription(),
+                meeting.getLocation(), meeting.getEventStatus(), meeting.getReminder());
+
+        ByteArrayResource bt = new ByteArrayResource(fileConfig.getBytes());
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.valueOf("application/ics"));
         headers.add("Content-Disposition", "attachment;filename=" + meeting.getSummary() + ".ics");
--- src/main/java/ics/StringBuilder.java
+++
@@ -1,4 +0,0 @@
-package ics;
-
-public class StringBuilder {
-}
